51ae6e2cbe0325d9a025edb083b10c4c028edbb9
IntegracionesVisaNet <55891395+IntegracionesVisaNet@users.noreply.github.com>
2021-05-06 21:53:12 -0500

    Final

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { NativeModules } from 'react-native';
 
 export default function App() {
+
+  const [value, onChangeText] = React.useState('1');
+
+  const showToast = async (amount) => {
+
+    var purchase = "23498458";
+
+    try {
+      var requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+          'Authorization': 'Basic Z2lhbmNhZ2FsbGFyZG9AZ21haWwuY29tOkF2MyR0cnV6'
+        },
+        redirect: 'follow'
+      };
+
+      fetch("https://apitestenv.vnforapps.com/api.security/v1/security", requestOptions)
+        .then(response => response.text())
+        .then(token =>
+          // console.log(result)
+          NativeModules.NiubizModule.payWithNiubiz(token, amount, purchase).then(niubizResponse => {
+            console.log('niubizResponse: ', niubizResponse);
+            var jsonResponse = JSON.parse(niubizResponse);
+            if (jsonResponse.dataMap != undefined) {
+              console.log('Mensaje: ', jsonResponse.dataMap.ACTION_DESCRIPTION);
+            } else {
+              console.log('Mensaje: ', jsonResponse.data.ACTION_DESCRIPTION);
+            }
+          })
+        )
+        .catch(error => console.log('error', error));
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ingrese importe</Text>
+      <TextInput keyboardType='numeric' style={{ height: 50, width: 150, borderColor: 'gray', borderWidth: 1 }} value={value} onChangeText={text => onChangeText(text)} />
+      <Button title="Pagar" onPress={() => showToast(value)} />
       <StatusBar style="auto" />
     </View>
   );
